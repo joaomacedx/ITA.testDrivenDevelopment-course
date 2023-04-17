eb4e55ab6c228fcd0579bbb4bda2605e1ce322f3
João Macêdo <joaomacedx@gmail.com>
2023-04-17 18:25:07 -0300

    refactor: create methods to apply validations

--- Projects/src/CamelCase/Implementation/CamelCase.java
+++ Projects/src/CamelCase/Implementation/CamelCase.java
@@ -12,9 +12,7 @@ public class CamelCase {
 
     public static List<String> toTransform(String value) {
         List<String> list = new ArrayList<String>();
-        if(isEmpty(value)) throw new EmptyStringCamelCaseException("This string is null or empty");
-        if(initWithNumber(value)) throw new InitWithNumberCamelCaseExeception("String cannot start with numbers");
-        if(hasEspecialCharacteres(value)) throw new HasEspecialCharCamelCaseException("String cannot start with especial characteres");
+        validations(value);
         list.add(value);
         return list;
     }
@@ -29,5 +27,10 @@ public class CamelCase {
     private static boolean hasEspecialCharacteres(String value) {
         if(value.substring(0).matches("[çÇ$&+,:;=?@#|'<>.^*()%!-].*")) return true;
         return false;
+    }
+    private static void validations(String value) {
+        if(isEmpty(value)) throw new EmptyStringCamelCaseException("This string is null or empty");
+        if(initWithNumber(value)) throw new InitWithNumberCamelCaseExeception("String cannot start with numbers");
+        if(hasEspecialCharacteres(value)) throw new HasEspecialCharCamelCaseException("String cannot start with especial characteres");
     }
 }
