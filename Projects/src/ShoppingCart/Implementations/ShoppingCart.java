dbb60a849421e7b4179b1eb587d2d24770bef9f4
João Macêdo <joaomacedx@gmail.com>
2023-05-17 05:54:39 -0300

    refactor: changing the implementation of the ShoppingCart class to meet testing requirements

--- Projects/src/ShoppingCart/Implementations/ShoppingCart.java
+++ Projects/src/ShoppingCart/Implementations/ShoppingCart.java
@@ -7,14 +7,15 @@ import ShoppingCart.Interfaces.ICartObserver;
 
 public class ShoppingCart {
     
-    private ICartObserver observer;
+    private List<ICartObserver> observersRepository = new ArrayList<>();
     private List<Product> productsRepository = new ArrayList<Product>();
     public ShoppingCart(List<Product> productsList) {
         this.productsRepository = productsList;
     }
     public void addProduct(Product productToAdd) {
         productsRepository.add(productToAdd);
-        if(observer!= null) this.observer.productAdded(productToAdd.productName(), productToAdd.productPrice());
+        for(ICartObserver observer: this.observersRepository) 
+            observer.productAdded(productToAdd.productName(), productToAdd.productPrice());
     }
     public int totalValueOfProductsInTheCart() {
         int totalValue = 0;
@@ -23,6 +24,6 @@ public class ShoppingCart {
         return totalValue;
     }
     public void addObserver(ICartObserver observerToAdd) {
-        this.observer = observerToAdd;
+        this.observersRepository.add(observerToAdd);
     }
 }
